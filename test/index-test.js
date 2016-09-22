053b0022330a903fc3e8be227324e961091e73ba
jaspercurry <jaspercurry@gmail.com>
2016-09-21 21:34:55 -0400

    read lab

--- test/index-test.js
+++ test/index-test.js
@@ -1,3 +1,4 @@
+"use strict";
 const expect = require('expect')
 
 const fs = require('fs')
@@ -6,13 +7,13 @@ const path = require('path')
 
 
 describe('index', () => {
-  
+
   jsdom({
     src: fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8')
   })
-  
+
 
   it('runs', () => {
-    expect(true).to.be.true
+    
   })
 })
